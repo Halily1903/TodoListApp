b39657022c627aa2db910523cf018f38e5f03530
Halily1903 <halilyildirim783@gmail.com>
2023-12-15 01:10:45 +0300

    Add project files.

---
+++ TodoListApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Entities;
+using TodoListApp.Models.EntityFramework;
+
+namespace TodoListApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Context _context;
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Todos.OrderByDescending(x=>x.Id).ToList();
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Index(string title)
+        {
+            Todo todo = new Todo();
+            todo.Title = title;
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var todo = _context.Todos.Where(x=>x.Id==id).FirstOrDefault();
+            _context.Todos.Remove(todo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TodoListApp/Models/EntityFramework/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApp.Entities;
+
+namespace TodoListApp.Models.EntityFramework
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+        }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+                
+        }
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
